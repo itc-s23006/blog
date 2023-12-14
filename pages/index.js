c75dc8196e52a78dfd89e16cf48c76cdc46af950
s23006 <s23006@std.it-college.ac.jp>
2023-12-14 01:48:18 +0000

    修正

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,11 @@ import Footer from '../components/footer'
 const Home = () => {
   return (
     <>
-      <header />
+      <Header />
       <main>
-        <hero />
+        <Hero />
       </main>
-      <footer />
+      <Footer />
     </>
   )
 }
