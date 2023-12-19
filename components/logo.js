26c9dff2a9e0d316b4feaef6ee23ad8180452cc3
s23006 <s23006@std.it-college.ac.jp>
2023-12-19 06:41:57 +0000

    logo.module.cssをインポート className属性でリンクにスタイルの設定を適用

--- components/logo.js
+++ components/logo.js
@@ -1,7 +1,12 @@
 import Link from 'next/link'
+import styles from 'styles/logo.module.css'
 
-const Logo = () => {
-  return <Link href='/'>CUBE</Link>
+const Logo = (boxOn = false) => {
+  return (
+    <Link href='/'>
+      <div className={boxOn ? styles.box : styles.basic}>CUBE</div>
+    </Link>
+  )
 }
 
 export default Logo
