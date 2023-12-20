882dc2dbbd67dee5a04b8b768e2c4766ff70f621
s23006 <s23006@std.it-college.ac.jp>
2023-12-20 06:01:31 +0000

    chapter4-3 classNameを追加

--- components/hero.js
+++ components/hero.js
@@ -1,13 +1,15 @@
 import styles from 'styles/hero.module.css'
 
-const Hero = ({ title, subtitle, imageOn = false }) => (
-  <div className={styles.flexContainer}>
-    <div className={styles.text}>
-      <h1 className={styles.title}>{title}</h1>
-      <p className={styles.subtitle}>{subtitle}</p>
+const Hero = ({ title, subtitle, imageOn = false }) => {
+  return (
+    <div className={styles.flexContainer}>
+      <div className={styles.text}>
+        <h1 className={styles.title}>{title}</h1>
+        <p className={styles.subtitle}>{subtitle}</p>
+      </div>
+      {imageOn && <figure> [image] </figure>}
     </div>
-    {imageOn && <figure> [image] </figure>}
-  </div>
-)
+  )
+}
 
 export default Hero
