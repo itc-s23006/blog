93b04a74aa5d72a96a363a8499dd74d9e701c651
s23006 <s23006@std.it-college.ac.jp>
2023-12-21 00:15:34 +0000

    本文コンテンツを追加

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,10 +1,11 @@
 import Hero from 'components/hero'
+import Container from 'components/container'
 const props = { title: 'Blog', subtitle: 'Recent Posts' }
 const Home = () => {
   return (
-    <>
+    <Container>
       <Hero {...props} />
-    </>
+    </Container>
   )
 }
 
