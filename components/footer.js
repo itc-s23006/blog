beaf7a67f29043d4833bea3eca8be5c97421ffb0
s23006 <s23006@std.it-college.ac.jp>
2024-01-05 11:24:06 +0000

    Socialコンポーネントをインポートして追加

--- components/footer.js
+++ components/footer.js
@@ -1,12 +1,14 @@
 import Logo from 'components/logo'
 import styles from 'styles/footer.module.css'
 import Container from 'components/container'
+import Social from 'components/social'
 const Footer = () => {
   return (
     <footer className={styles.wrapper}>
       <Container>
         <div className={styles.flexContainer}>
           <Logo />
+          <Social />
         </div>
       </Container>
     </footer>
