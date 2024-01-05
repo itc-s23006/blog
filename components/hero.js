2b9e5015e5458904a9c153c4ea02b4cef2a9b7e0
s23006 <s23006@std.it-college.ac.jp>
2024-01-05 06:34:17 +0000

    画像を表示する

--- components/hero.js
+++ components/hero.js
@@ -1,11 +1,18 @@
 import styles from 'styles/hero.module.css'
+import Image from 'next/legacy/image'
+import cube from 'images/cube.jpg'
+
 const Hero = ({ title, subtitle, imageOn = false }) => (
   <div className={styles.flexContainer}>
     <div className={styles.text}>
       <h1 className={styles.title}>{title}</h1>
       <p className={styles.subtitle}>{subtitle}</p>
     </div>
-    {imageOn && <figure> [image] </figure>}
+    {imageOn && (
+      <figure>
+        <Image src={cube} alt='' layout='responsive' />
+      </figure>
+    )}
   </div>
 )
 
