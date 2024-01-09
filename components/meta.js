1e88daa387916ab19798bf2863bb3abcdf5c3702
s23006 <s23006@std.it-college.ac.jp>
2024-01-09 01:51:13 +0000

    ページタイトル以外のメタデータを追加する

--- components/meta.js
+++ components/meta.js
@@ -1,15 +1,19 @@
 import Head from 'next/head'
 import { siteMeta } from 'lib/constants'
 
-const { siteTitle } = siteMeta
+const { siteTitle, siteDesc } = siteMeta
 
-const Meta = ({ pageTitle }) => {
+const Meta = ({ pageTitle, pageDesc }) => {
   const title = pageTitle ? `${pageTitle} | ${siteTitle}` : siteTitle
+  const desc = pageDesc ?? siteDesc
 
   return (
     <Head>
       <title>{title}</title>
       <meta property='og:title' content={title} />
+
+      <meta name='description' content={desc} />
+      <meta property='og:description' content={desc} />
     </Head>
   )
 }
