3463b508e49f4e515de9d2df7aca6fa9723c4e50
s23006 <s23006@std.it-college.ac.jp>
2024-01-09 01:37:04 +0000

    ページタイトルが未指定な場合の処理を指定する

--- components/meta.js
+++ components/meta.js
@@ -1,15 +1,17 @@
 import Head from 'next/head'
-
 import { siteMeta } from 'lib/constants'
+
 const { siteTitle } = siteMeta
 
-const Meta = ({ pageTitle }) => (
-const title = pageTitle ? `${pageTitle} | ${siteTitle}` : siteTitle
+const Meta = ({ pageTitle }) => {
+  const title = pageTitle ? `${pageTitle} | ${siteTitle}` : siteTitle
 
-  <Head>
-    <title>{title}</title>
-    <meta property='og:title' content={title} />
-  </Head>
-)
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta property='og:title' content={title} />
+    </Head>
+  )
+}
 
 export default Meta
