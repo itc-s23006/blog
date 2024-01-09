ea03c677c4e7fa6f503e706c3d278791b5cb46a5
s23006 <s23006@std.it-college.ac.jp>
2024-01-09 02:24:01 +0000

    サイトに関する情報とアイコン画像を追加する

--- components/meta.js
+++ components/meta.js
@@ -2,7 +2,14 @@ import Head from 'next/head'
 import { siteMeta } from 'lib/constants'
 import { useRouter } from 'next/router'
 
-const { siteTitle, siteDesc, siteUrl } = siteMeta
+const {
+  siteTitle,
+  siteDesc,
+  siteUrl,
+  siteType,
+  siteLocale,
+  siteIcon
+} = siteMeta
 
 const Meta = ({ pageTitle, pageDesc }) => {
   const title = pageTitle ? `${pageTitle} | ${siteTitle}` : siteTitle
@@ -20,6 +27,13 @@ const Meta = ({ pageTitle, pageDesc }) => {
 
       <link rel='canonical' href={url} />
       <meta property='og:url' content={url} />
+
+      <meta property='og:site_name' content={siteTitle} />
+      <meta property='og:type' content={siteType} />
+      <meta property='og:locale' content={siteLocale} />
+
+      <link rel='icon' href={siteIcon} />
+      <link rel='apple-touch-icon' href={siteIcon} />
     </Head>
   )
 }
