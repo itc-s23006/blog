f67c03a61ad6234491729c5f5602fe1207617475
s23006 <s23006@std.it-college.ac.jp>
2024-01-09 03:51:11 +0000

    siteImgをインポート

--- components/meta.js
+++ components/meta.js
@@ -10,12 +10,17 @@ const {
   siteLocale,
   siteIcon
 } = siteMeta
+import siteImg from 'images/ogp.jpg'
 
-const Meta = ({ pageTitle, pageDesc }) => {
+const Meta = ({ pageTitle, pageDesc, pageImg, pageImgW, pageImgH }) => {
   const title = pageTitle ? `${pageTitle} | ${siteTitle}` : siteTitle
   const desc = pageDesc ?? siteDesc
   const router = useRouter()
   const url = `${siteUrl}${router.asPath}`
+  const img = pageImg || siteImg.src
+  const imgW = pageImgW || siteImg.width
+  const imgH = pageImgH || siteImg.height
+  const imgUrl = img.startsWith('https') ? img : `${siteUrl}${img}`
 
   return (
     <Head>
@@ -34,6 +39,11 @@ const Meta = ({ pageTitle, pageDesc }) => {
 
       <link rel='icon' href={siteIcon} />
       <link rel='apple-touch-icon' href={siteIcon} />
+
+      <meta property='og:image' content={imgUrl} />
+      <meta property='og:image:width' content={imgW} />
+      <meta property='og:image:height' content={imgH} />
+      <meta name='twitter:card' content='summary_large_image' />
     </Head>
   )
 }
