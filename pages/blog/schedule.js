6b9004ae6cede59752e39e8ef0ad3bb73cf3da47
s23006 <s23006@std.it-college.ac.jp>
2024-01-12 00:53:59 +0000

    eyecatchLocalをインポート、eyecatch:の部分変更

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -12,6 +12,7 @@ import ConvertBody from 'components/convert-body'
 import PostCategories from 'components/post-categories'
 import { extractText } from 'lib/extract-text'
 import Meta from 'components/meta'
+import { eyecatchLocal } from 'lib/constants'
 
 const Schedule = ({
   title,
@@ -64,12 +65,14 @@ const getStaticProps = async () => {
 
   const description = extractText(post.content)
 
+  const eyecatch = post.eyecatch ?? eyecatchLocal
+
   return {
     props: {
       title: post.title,
       publish: post.publishDate,
       content: post.content,
-      eyecatch: post.eyecatch,
+      eyecatch: eyecatch,
       categories: post.categories,
       description
     }
