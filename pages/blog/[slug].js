5fae4779c2817cd7139c24fcb7f3a8c96538174a
s23006 <s23006@std.it-college.ac.jp>
2024-01-16 05:56:24 +0000

    関数を使って前後の記事のタイトルとスラッグを取り出す import { prevNextPost }

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -14,6 +14,7 @@ import { extractText } from 'lib/extract-text'
 import Meta from 'components/meta'
 import { eyecatchLocal } from 'lib/constants'
 import { getPlaiceholder } from 'plaiceholder'
+import { prevNextPost } from 'lib/prev-next-post'
 
 const Post = ({
   title,
@@ -83,6 +84,9 @@ const getStaticProps = async context => {
   const { base64 } = await getPlaiceholder(eyecatch.url)
   eyecatch.blurDataURL = base64
 
+  const allSlugs = await getAllSlugs()
+  const [prevPost, nextPost] = prevNextPost(allSlugs, slug)
+
   return {
     props: {
       title: post.title,
@@ -90,7 +94,9 @@ const getStaticProps = async context => {
       content: post.content,
       eyecatch,
       categories: post.categories,
-      description
+      description,
+      prevPost,
+      nextPost
     }
   }
 }
