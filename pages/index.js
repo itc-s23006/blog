2fcd5cfac9b5316a7d06a0f6107791f094709b89
s23006 <s23006@std.it-college.ac.jp>
2024-01-17 00:09:08 +0000

    トップページに記事一覧を表示する。

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,42 @@
 import Hero from 'components/hero'
 import Container from 'components/container'
 import Meta from 'components/meta'
+import { getAllPosts } from 'lib/api'
+import Posts from 'components/posts'
+import Pagination from 'components/pagination'
+import { getPlaiceholder } from 'plaiceholder'
+import { eyecatchLocal } from 'lib/constants'
 
-const props = {
-  title: 'CUBE',
-  subtitle: 'アウトプットしていくサイト',
-  imageOn: true
-}
-const Home = () => {
+const Home = ({ posts }) => {
   return (
     <>
       <Container>
         <Meta />
         <Hero {...props} />
-      </Container>{' '}
+        <Posts posts={posts} />
+        <Pagination nextUrl='/blog' nextText='MorePosts' />
+      </Container>
     </>
   )
 }
 
+const getStaticProps = async () => {
+  const posts = await getAllPosts(4)
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty.call('eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const { base64 } = await getPlaiceholder(post.eyecatch.url)
+    post.eyecatch.blurDataURL = base64
+  }
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
+
 export default Home
+export { getStaticProps }
